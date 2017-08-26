c7717659a5e02ce1be45955c79ec45ad1f11d5ce
Ola Popp <olapopp85@gmail.com>
2017-08-26 08:28:32 +0200

    Add project files.

---
+++ FF.Service/BaseService.cs
@@ -0,0 +1,16 @@
+﻿using NLog;
+
+
+namespace FF.Service
+{
+    public class BaseService
+    {
+        protected Logger Log { get; private set; }
+
+        protected BaseService()
+        {
+            Log = LogManager.GetLogger(GetType().FullName);
+        }
+
+    }
+}
